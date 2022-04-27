25b9b74e5301e8a0df6bc41c38c87376ad3725cc
Zakhar <ukhanovza@gmail.com>
2022-04-27 23:15:35 +0300

    add adaptive for movie pages

--- src/components/PageWithFilms.js
+++ src/components/PageWithFilms.js
@@ -1,40 +1,57 @@
 import React from "react";
-import './css-comp/pagefilms.css'
+import "./css-comp/pagefilms.css";
 
 const PageFilms = () => {
-    return(
-        <div className="page_films_items">
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
-            <div className="film_item">
-            </div>
+  return (
+    <div className="films_item">
+      <div className="page_films_items">
+        <div className="film_item">
+          <p>Название</p>
         </div>
-    )
-}
-export default PageFilms
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+        <div className="film_item">
+          <p>Название</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PageFilms;
