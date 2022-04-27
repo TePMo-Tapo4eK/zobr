b22dff6eabb5311ebda8376552a06a3870fc3367
Zakhar <ukhanovza@gmail.com>
2022-04-27 21:22:06 +0300

    add adaptive without photos

--- src/components/MainPageContent.js
+++ src/components/MainPageContent.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./css-comp/MainPageContent.css"
-import Img1 from './images/1.jpg'
-import Img2 from './images/2.webp'
-import Img3 from './images/3.jpg'
-import Img4 from './images/4.webp'
 import { Link } from "react-router-dom";
 
 const MainPageContent = () => {
   return (
-    <div className="main_pahe_content">
+    <div className="main_page_content">
         <div className="page_text">
-            <p className="title">Смотри <p className="title_p">фильмы</p></p>
+            <h1 className="title">Смотри <p className="title_p">фильмы</p></h1>
             <p className="title">вместе с друзьями!</p>
             <ul className="page_btns">
                 <li><Link to='/create' className="btn">Создать комнату</Link></li>
@@ -19,12 +15,6 @@ const MainPageContent = () => {
             </ul>
         </div>
         <div className="page_photos">
-            <img alt='img' src={Img1} className="main_page_img one"/>
-            <img alt='img' src={Img2} className="main_page_img two"/>
-            <img alt='img' src={Img3} className="main_page_img three"/>
-            <img alt='img' src={Img4} className="main_page_img four"/>
-            <img alt='img' src={Img1} className="main_page_img one"/>
-            <img alt='img' src={Img2} className="main_page_img two"/>
         </div>
     </div>
   );
